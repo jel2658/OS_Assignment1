5d2d8b36c6919e7e77283cee7973ccd926206de0
Justin L <78121936+jel2658@users.noreply.github.com>
2021-10-28 14:02:19 -0400

    Update consumer.c

--- consumer.c
+++ consumer.c
@@ -20,17 +20,17 @@ int main() {    // Decent portion of code borrowed from github so that I could m
     const int FILESIZ = 5000;   // The size of the file that will be shared
 
     int shm_fd = shm_open(name, O_RDWR, 0666);  // Opens up the shared file.
-    if (shm_fd == -1) { // Checks to make sure the file has actually been opened
+    /*if (shm_fd == -1) { // Checks to make sure the file has actually been opened
         printf("Failed to shm_open.");
         exit(1);
-    }
+    }*/
 
     //int* q2 = mmap(0, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     char *base_address = mmap(0, FILESIZ, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0); // Open up the shared memory address?
-    if (base_address == MAP_FAILED) {   // Checks to make sure the address was opened.
+    /*if (base_address == MAP_FAILED) {   // Checks to make sure the address was opened.
         printf("Failed to map.");
         exit(1);
-    }
+    }*/
     display("Consumer", base_address, 64); // The helper function, display... outputs what's in the shared memory
     printf("%s", base_address);
 
@@ -46,10 +46,9 @@ int main() {    // Decent portion of code borrowed from github so that I could m
         printf("Failed to unmap.");
         exit(1);
     }
-
-    close(shm_fd);  // Close the shared memory address
-
+    close(shm_fd);      // Close the shared memory address.
     shm_unlink(name);   // Unlink the shared memory.
+    
 
     /*sem_t* produce = sem_open("produce", O_CREAT, 0666, 0); // This code made me confused and didn't work, but I'll keep it for documentation.
     sem_t* check = sem_open("ready", O_CREAT, 0666, 2);
